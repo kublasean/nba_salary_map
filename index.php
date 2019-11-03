d834e83694fd9ecaf01e88fc6883f384e0568ecd
EC2 Default User <se@nwhal3n.net>
2019-11-03 21:57:48 +0000

    idk

--- index.php
+++ index.php
@@ -47,10 +47,8 @@ rect:hover {
     width: 100%;
     position: relative;
     overflow: hidden;
-}
-.body-container {
     line-height: 0.5;
-    position: relative;
+    background-color: white;
 }
 .control:hover {
     cursor: pointer;
@@ -64,11 +62,9 @@ h1 {
 }
 
 @media only screen and (max-width: 600px) {
-    .body-container {
-        width: 200%;
-    }
     .chart-container {
         height: 1000px;
+        width: 200%;
     }
     .control {
         margin-right: 10px;
@@ -83,14 +79,28 @@ h1 {
 <body class="base-color">
     <?php $_GET['section'] = "nba"; include('../../../cgi-bin/navbar.php'); ?>
     <div class="body-container foreground-color">
-        <span style="padding-left: 1px;" class="control" onclick="prev_year();">prev</span>
-        <h1 style="text-align: center; display: inline-block;"><b id="year">2019-20</b> NBA Contracts</h1>
+        <h1 style="title">Who's on the books? NBA contracts 2019-2025</h1>
+        <span style="padding-left: 1px;" class="control" onclick="prev_year();">previous</span>
+        <h1 style="text-align: center; display: inline-block;"><b id="year">2019-20</b></h1>
         <span class="control" onclick="next_year();">next</span>
         <div class="chart-container">
             <div class="tooltip">hello there</div>
             <div id="title_layer"></div>
             <div id="salary_treemap"></div>
         </div>
+        <h3>about</h3>
+        <p>Each rectangle represents a player and is scaled by that player's contract size for the
+        selected year. Players are grouped by their teams, indicated by their color and abbreviation.
+        Press <b>previous</b> or <b>next</b> to step through available seasons. Hover (or click on mobile) over a player to 
+        see their name and contract in millions or thousands of dollars. Note: the scale can
+        change dramatically between years since the chart is configured to make use of the entire space no 
+        matter how many contracts exist for that year.</p>
+        <!---
+        <h2>Tutorial</h2>
+        <p>This type of data visualization is called a treemap. The only place I'd seen treemaps before 
+        embarking on this project was in software for breaking down disk usage.     They are similar to pie charts,
+        but more effective for comparison between "parts" rather than "parts" to the whole. 
+        It's certainly easier for me at least to compare rectangles than slices.</p>--->
     </div>
 <script>
 var ind = 0;
